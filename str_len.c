c8d5054dcc9747ff81ab2123f1ccead35911396b
Bha-vani <85944086+Bha-vani@users.noreply.github.com>
2021-06-28 22:23:32 +0530

    Create str_len.c

---
+++ str_len.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    char ch[50] = "keep smile";
+    printf("Entered String is:");
+    int len
+        = printf("%s\n", ch);
+    printf("Length is:");
+    printf("%d", len - 1);
+}
+
+#if 0
+Entered String is:Naveen bittu
+Length is:10
+#endif
